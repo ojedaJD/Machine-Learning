647cf2c338ddfc021eddb43e6f7d80ee36c6d2fd
JD Ojeda <89946133+ojedaJD@users.noreply.github.com>
2023-04-19 21:13:42 -0400

    Add files via upload

---
+++ KNN Working File.py
@@ -0,0 +1,41 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv('car.data')
+print(data.head())
+
+#transforming all irregular data into integers
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+clss = le.fit_transform(list(data["class"]))
+
+predict = "class"
+
+X = list(zip(buying,maint,door,persons,lug_boot,safety))
+y = list(clss)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+#setting k greater than 9, begins to worsen performance
+model = KNeighborsClassifier(n_neighbors=9)
+
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(x_test)):
+    print("predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 9, True)
+    print("N: ", n)
